db13ec414d1ccffc4c4b6a4457e2ca3a4159b405
Peter Dahlin <peterdaliandalledahlin@gmail.com>
2022-09-27 19:29:03 +0200

    update collection on click and change

--- src/components/pages/LaundryroomList.vue
+++ src/components/pages/LaundryroomList.vue
@@ -1,7 +1,7 @@
 <template>
     <v-container>
         <h1>laundryroomlist</h1>
-        <div v-for="laundryroom in storedRegions" :key="laundryroom.id">
+        <div v-for="laundryroom in region.laundryrooms" :key="laundryroom.id">
             <v-sheet
               elevation="8"
               class="mx-auto mb-5"
@@ -13,8 +13,9 @@
                     :label="`${laundryroom.name} upptagen`"
                     color="red"
                     hide-details
-                    @change="updateOccupiedLandry(laundryroom.id, laundryroom.occupied)"
+                    @change="syncToStorage(), closePanel(laundryroom.id, laundryroom.occupied)"
                 ></v-checkbox>
+
                 <v-list class="pa-0">
                 <v-list-item>
                     <template v-slot:prepend>
@@ -28,35 +29,35 @@
                     </template>
                 </v-list-item>
                 </v-list>          
-                <v-expansion-panels variant="accordion">
-                <v-expansion-panel :disabled="laundryroom.occupied">
-                    <v-expansion-panel-title>
-                    {{ laundryroom.name}}
-                    </v-expansion-panel-title>
-                    <v-expansion-panel-text v-for="task in laundryroom.tasks" :key="task.id">
-                    <v-list>
-                        <v-list-item
-                        :key="task.id"
-                        :title="task.name"
-                        >
-                        <template v-slot:prepend>
-                            <v-avatar color="grey-lighten-1">
-                            <v-img :src="task.img" height="100%" cover></v-img>
-                            </v-avatar>
-                        </template>
-        
-                        <template v-slot:append>
-                            <v-btn
-                            @click="task.done = !task.done, updateAssignmentTask(laundryroom.id, task.id, task.done)"
-                            :color="task.done ? 'green' : 'red'"
-                            :icon="task.done ? 'mdi-check' : 'mdi-close'"
-                            size="x-small"
-                            ></v-btn>
-                        </template>
-                        </v-list-item>
-                    </v-list>
-                    </v-expansion-panel-text>
-                </v-expansion-panel>
+                <v-expansion-panels v-model="panels" multiple variant="accordion">
+                    <v-expansion-panel :value="laundryroom.id" :disabled="laundryroom.occupied">
+                        <v-expansion-panel-title>
+                        {{ laundryroom.name}}
+                        </v-expansion-panel-title>
+                        <v-expansion-panel-text v-for="task in laundryroom.tasks" :key="task.id">
+                        <v-list>
+                            <v-list-item
+                            :key="task.id"
+                            :title="task.name"
+                            >
+                            <template v-slot:prepend>
+                                <v-avatar color="grey-lighten-1">
+                                <v-img :src="task.img" height="100%" cover></v-img>
+                                </v-avatar>
+                            </template>
+            
+                            <template v-slot:append>
+                                <v-btn
+                                @click="task.done = !task.done, syncToStorage()"
+                                :color="task.done ? 'green' : 'red'"
+                                :icon="task.done ? 'mdi-check' : 'mdi-close'"
+                                size="x-small"
+                                ></v-btn>
+                            </template>
+                            </v-list-item>
+                        </v-list>
+                        </v-expansion-panel-text>
+                    </v-expansion-panel>
                 </v-expansion-panels>
             </v-sheet>
         </div><!--loop storedRegions-->
@@ -64,44 +65,42 @@
 </template>
   
 <script setup>
-import { ref, onMounted } from 'vue'
-import { useRoute } from 'vue-router'
-import key from '../../assets/key.png'
+//IMPORTS
+    import { ref, onMounted } from 'vue'
+    import { useRoute } from 'vue-router'
+    import key from '../../assets/key.png'
+    import Localbase from 'localbase'
 
-import Localbase from 'localbase'
-let db = new Localbase('db')
-  
+//LOCALBASE
+    let db = new Localbase('db')
+
+//GET ROUTE PARAMS
 const route = useRoute()
 
-let storedRegions = ref([])
+//GET REGION
+let region = ref([])
 
-const updateOccupiedLandry = async (laundryroomId, laundryroomOccupied) => {
-    const laundryRoom = await db.collection('regions').doc({ id: laundryroomId}).get()
-    for( let i = 0; i < laundryRoom.laundryrooms.length; i++) {
-        if(laundryRoom.laundryrooms[i].id == laundryroomId) {
-            laundryRoom.laundryrooms[i].occupied = laundryroomOccupied
+//CLOSE EXPANSION PANEL ON CHECKBOX LAUNDRY OCCUPIED
+let panels = ref([])
+const closePanel = (id, occupied) => {
+    if(occupied) {
+        for( let i = 0; i < panels.value.length; i++) {
+            if(panels.value[i] == id) {
+                return panels.value =  panels.value.filter(e => e !== id); // will return ['A', 'C']
+            }
         }
-   }
-   db.collection('regions').doc({ id: laundryroomId }).set(laundryRoom)
+    }
 }
 
-const updateAssignmentTask = async (laundryroomId, taskId, done) => {
-  const laundryRoom = await db.collection('regions').doc({ id: laundryroomId}).get()
-  console.log(laundryRoom)
-   for( let i = 0; i < laundryRoom.laundryrooms.tasks.length; i++) {
-    
-    if(laundryRoom.laundryrooms.tasks[i].id == taskId) {
-      laundryRoom.laundryrooms.tasks[i].done = done
-    }
-   }
-  db.collection('regions').doc({ id: laundryroomId }).set(laundryRoom)
+//SET REGIONS COLLECTION ON CLICK & CHANGE
+const syncToStorage = async () => {
+  const data = await db.collection('regions').doc({id: region.value.id}).set(JSON.parse(JSON.stringify(region.value)))
 }
-  
+
+//GET STORED REGION FROM ROUTE PARAMS LOOP TO DOM 
 onMounted( () => {
-    db.collection('regions').doc({ id: parseInt(route.params.id) }).get().then(regions => {
-        //console.log(regions.laundryrooms)
-        storedRegions.value = regions.laundryrooms
+    db.collection('regions').doc({ id: parseInt(route.params.id) }).get().then(storedRegion => {       
+        region.value = storedRegion
     })
 })
 </script>
-
